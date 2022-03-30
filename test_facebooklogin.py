02c88ff09dedda0c264e02c824b73e8f39b167b3
Shobhan S <shobhan.11.2000@gmail.com>
2022-03-30 17:39:30 +0530

    Selenium training day1.

--- test_facebooklogin.py
+++ test_facebooklogin.py
@@ -4,12 +4,14 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
+username=input("Enter use name")
+password=input("Enter the pass")
 print("test case started")
 driver.maximize_window()
 driver.get("https://www.facebook.com/")
-driver.find_element_by_name("email").send_keys("Harman")
+driver.find_element_by_name("email").send_keys(username)
 time.sleep(1)
-driver.find_element_by_name("pass").send_keys("123456789")
+driver.find_element_by_name("pass").send_keys(password)
 time.sleep(1)
 driver.find_element_by_name("login").click()
 time.sleep(3)
